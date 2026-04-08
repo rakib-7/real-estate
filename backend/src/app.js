d5e5cf4d4f8e748e7d75fcdb58a162da03307599
rakib-7 <115614708+rakib-7@users.noreply.github.com>
2026-04-08 17:36:53 +0600

    Change server listen address to 0.0.0.0

--- backend/src/app.js
+++ backend/src/app.js
@@ -86,6 +86,6 @@ app.use((err, req, res, next) => {
 // --- SERVER STARTUP ---
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
